078548dccc797cbf65c717d1c12628f38c7cdec0
15_2051063556_Vũ Hoàng Giang <92315083+hoanggiang28082002@users.noreply.github.com>
2022-01-07 19:57:47 +0700

    Add files via upload

---
+++ DangNhap/Amazon/App/home.php
@@ -0,0 +1,393 @@
+<?php
+      session_start();
+   if(!isset($_SESSION['id']) &&  !isset($_SESSION['name'])  && !isset($_SESSION['password'])){
+      header("Location: home-wt.php");
+    } 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Amazon.com. Spend less. Smile more.</title>
+    <link rel="stylesheet" href="../css/home.css">
+    
+    <link rel="shortcut icon" href="../images/logo-favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body class="my-body">
+    <!-- HEADER -->
+          <div class="header d-flex">
+                 <div class="header-left d-flex">
+                      <img src="../images/logo.png" alt="" style="cursor:pointer;">
+                         <div class="texth9" style="cursor:pointer;">
+                           <img src="../images/outline_location_on_white_24dp.png" alt="">
+                           <div class="deliver style="cursor:pointer;">
+                              <h9>Deliver to</h9>
+                              <h10>Vietnam</h10>
+                            </div>
+                         </div>   
+                 </div>
+                 <div class="header-middle">
+                       <div class="box-mix d-flex">
+                            <button>All<i class="fas fa-caret-down"></i></button>
+                            <input type="text">
+                            <div class="btn-search" >
+                               <button><i class="fas fa-search" style=margin-left:6px></i></button>
+                            </div>
+                       </div>
+                 </div>
+                 <div class="header-right d-flex">
+                        <div class="logo-american d-flex "style="cursor:pointer;">
+                           <img src="../images/logo2.png" alt="">
+                           <button><i class="fas fa-caret-down"></i></button>
+                        </div>
+                        <div class="signin "style="cursor:pointer">
+                           <h9>Hello,<?php echo $_SESSION['name']; ?></h9>
+                           <div class="text-account d-flex">
+                              <h10>Account & Lists</h10>
+                              <button onclick="menuToggle();"><i class="fas fa-caret-down"></i></button>
+                           </div>
+                        </div>
+                        <div class="order" style="cursor:pointer">
+                           <h9>Returns</h9>
+                           <h10>& Orders</h10>
+                        </div>
+                        <div class="cart d-flex" style="cursor:pointer">
+                           <i class="fas fa-baby-carriage"></i>
+                           <h10>Cart</h10>
+                        </div>
+                 </div>
+          </div>
+
+ <!-- HEADER -->
+
+
+<!-- HEADER DOWN -->
+          <div class="header-down w-100 d-flex" style="cursor:pointer">
+                <div class="menu-icon d-flex">
+                  <button><i class="fas fa-bars"></i></button>
+                  <h10>All</h10>
+                </div>
+                <div class="menu-icon" style="width:10%;">
+                    <h10>Today's Deals</h10>
+                </div>
+                <div class="menu-icon" style="width:12%;">
+                  <h10>Customer Service</h10>
+                </div>
+                <div class="menu-icon" style="width:6%;">
+                   <h10>Registry</h10>
+                </div>
+                <div class="menu-icon" style="width:8%;">
+                  <h10>Gift Cards</h10>
+                </div>
+                <div class="menu-icon" style="width:4%;">
+                  <h10>Sell</h10>
+                </div>
+                <div class="menu-icon" style="width:15%;margin-left: 35rem;">
+                   <h10>New Years,new deals</h10>
+                </div>
+          </div>
+ <!-- HEADER DOWN -->
+
+
+
+
+<!-- CAROSEL -->
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+   <div class="carousel-inner">
+     <div class="carousel-item active">
+       <img src="../images/ad.jpg" class="d-block w-100" alt="...">
+     </div>
+     <div class="carousel-item">
+       <img src="../images/holiday.jpg" class="d-block w-100" alt="...">
+     </div>
+     <div class="carousel-item">
+       <img src="../images/shopall.jpg" class="d-block w-100" alt="...">
+     </div>
+   </div>
+   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+     <span class="carousel-control-prev-icon fs-5" aria-hidden="true"></span>
+   </button>
+   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+     <span class="carousel-control-next-icon fs-5" aria-hidden="true"></span>
+   </button>
+ </div>
+
+ 
+<!-- CAROSEL -->
+
+
+
+
+<!-- LEST SHOP -->
+
+<div class="my-container">
+    <div class="my-row">
+       <div class="box-img">
+          
+                <div class="gr-img cursor" >
+                   <h6>Shop by Category</h6>
+                   <img src="https://www.dangquangwatch.vn/upload/product/784845121_donghothuysy13.jpg" alt="">
+                   <img src="https://xuonggiay.vn/app/webroot/upload/image/images/giay-da-bo-nam.jpg" alt="">
+                   <img src="https://media3.scdn.vn/img2/2017/9_15/JJQWZ2_simg_de2fe0_500x500_maxb.jpg" alt="">
+                   <img src="https://jes.edu.vn/wp-content/uploads/2019/12/Kh%C4%83n-cho%C3%A0ng-L%E1%BB%A4A-T%C6%A0-T%E1%BA%B0M-D%E1%BB%86T-TR%C6%A0N-TO106.jpg" alt="">
+                   <div class="shop-now">
+                     <a href="">Shop now</a>
+                   </div>
+                </div>
+       
+      
+               <div class="gr-img">
+                  <h6>Gaming accessories</h6>
+                  <img src="https://bizweb.dktcdn.net/100/332/895/files/ao-khoac-long-1.png?v=1584782401997" alt="">
+                  <img src="https://salt.tikicdn.com/cache/400x400/ts/product/2f/ce/69/2d0023cba19ea9c6d39133ee94b45e16.png" alt="">
+                  <img src="http://dangcapquyong.com/images/product/vi-nam-cam-tay-polo-pl02-1514459247_Untitled-1.jpg" alt="">
+                  <img src="https://picassovietnam.vn/wp-content/uploads/ban-viet-picasso-918-chinh-hang-gia-bao-nhieu-2.jpg" alt="">
+                  <div class="shop-now">
+                     <a href="">Shop now</a>
+                   </div>
+               </div>
+   
+       
+               <div class="gr-img">
+                  <h6>Electronics</h6>
+                  <img src="https://5.imimg.com/data5/SELLER/Default/2020/12/OC/IN/IS/13508457/dell-laptop-500x500.jpg" alt="">
+                  <img src="https://a-static.mlcdn.com.br/618x463/iphone-12-apple-64gb-preto-tela-oled-61-5g-camera-dupla-12mp-ios/zoo/896242/81977af85bd280ad1a3a8125b88bb21b.jpg" alt="">
+                  <img src="https://salt.tikicdn.com/ts/product/b8/5e/83/840b63ede8209dadd0bb74147c5695a1.jpg" alt="">
+                  <img src="https://baohanhiphone.com.vn/upload/sanpham/large/126-goi-gui-bao-hanh-may-tinh-bang-ipad-pro-11-inch-1.jpg" alt="">
+                  <div class="shop-now">
+                     <a href="">Shop now</a>
+                   </div>
+               </div>
+   
+     
+               <div class="gr-img">
+               <h6>Food Japan</h6>
+                  <img style="width: 265px;height: 300px;" src="https://vn.bondlingo.tv/blog/wp-content/uploads/2020/07/fadya-azhary-5KS7T3Gs3CA-unsplash.jpg" alt="">
+                  <div class="shop-now">
+                     <a href="">Shop now</a>
+                   </div>
+               </div>
+         
+       </div>
+       <div class="box-img">
+          
+         <div class="gr-img">
+            <h6>Shop by Category</h6>
+            <img src="../images/xedap.jpg" alt="">
+            <img src="../images/xedap.jpg" alt="">
+            <img src="../images/xedap.jpg" alt="">
+            <img src="../images/xedap.jpg" alt="">
+            <div class="shop-now">
+              <a href="">Shop now</a>
+            </div>
+         </div>
+
+
+        <div class="gr-img">
+           <h6>Gaming accessories</h6>
+           <img src="../images/xedap.jpg" alt="">
+           <img src="../images/xedap.jpg" alt="">
+           <img src="../images/xedap.jpg" alt="">
+           <img src="../images/xedap.jpg" alt="">
+           <div class="shop-now">
+              <a href="">Shop now</a>
+            </div>
+        </div>
+
+
+        <div class="gr-img">
+           <h6>Electronics</h6>
+           <img src="../images/xedap.jpg" alt="">
+           <img src="../images/xedap.jpg" alt="">
+           <img src="../images/xedap.jpg" alt="">
+           <img src="../images/xedap.jpg" alt="">
+           <div class="shop-now">
+              <a href="">Shop now</a>
+            </div>
+        </div>
+
+
+        <div class="gr-img">
+           <img src="../images/xedap.jpg" alt="">
+           <div class="shop-now">
+              <a href="">Shop now</a>
+            </div>
+        </div>
+  
+</div>
+    </div>
+</div>
+
+
+<!-- LEST SHOP -->
+
+
+
+<!-- BOX-DOWN -->
+<div class="box-popup">
+   <div class="list-account">
+      <div class="list">
+         <h9>Your Lists</h9>
+         <ul>
+            <li><a href="">Create a List</a></li>
+            <li><a href="">Find a List or Registry</a></li>
+            <li><a href="">AmazonSmile Charity Lists</a></li>
+         </ul>
+      </div>
+      <div class="account">
+       <h9>Your Acount</h9>
+       <ul>
+          <li><a href="">Account</a></li>
+          <li><a href="">Order</a></li>
+          <li><a href="">Recommendations</a></li>
+          <li><a href="">Browsing History</a></li>
+          <li><a href="">Watchlist</a></li>
+          <li><a href="">Video Purchases & Rentals</a></li>
+          <li><a href="">Kindle Unlimited</a></li>
+          <li><a href="">Content & Devices</a></li>
+          <li><a href="">Subscribe & Save Items</a></li>
+          <li><a href="">Membership & Subscriptions</a></li>
+          <li><a href="">Music Library</a></li>
+          <li><a href="./home-wt.php">Sign Out</a></li>
+       </ul>
+    </div>
+   </div>
+</div>
+<!-- BOX-DOWN -->
+
+
+<footer>
+      <div class="footer">
+             <div class="footer-signin">
+                
+             </div>
+             <div class="back-to-top">
+                <input type="button" value="Back to top"> 
+             </div>
+             <div class="footer-info d-flex">
+                 <div class="footer-info-text">
+                    <h5>Get to Know Us</h5>
+                    <a href="">Careers</a>
+                    <a href="">Blog</a>
+                    <a href="">Investor Relations</a>
+                    <a href="">Amazon Devices</a>
+                 </div>
+                 <div class="footer-info-text">
+                  <h5>Make Money with Us</h5>
+                  <a href="">Sell products on Amazon</a>
+                  <a href="">Sell on Amazon Business</a>
+                  <a href="">Sell apps on Amazon</a>
+                  <a href="">Become an Affiliate</a>
+                  <a href="">Advertise Your Products</a>
+                  <a href="">Self-Publish with Us</a>
+                  <a href="">Host an Amazon Hub</a>
+                  <a href="">See More Make Money</a>
+               </div>
+               <div class="footer-info-text" style="padding-left: 2rem;">
+                  <h5>Amazon Payment Product</h5>
+                  <a href="">Amazon Business Card</a>
+                  <a href="">Shop with Point</a>
+                  <a href="">Reload Your Balance</a>
+                  <a href="">Amazon Currency</a>
+                  <a href="">Converter</a>
+               </div>
+               <div class="footer-info-text" style="padding-left: 3rem;">
+                  <h5>Let Us Help You</h5>
+                  <a href="">Amazon COVID-19</a>
+                  <a href="">Your Account</a>
+                  <a href="">Your Orders</a>
+                  <a href="">Shipping Rates & Policies</a>
+                  <a href="">Returns & Replacements</a>
+                  <a href="">Manage Your Content and Devices</a>
+                  <a href="">Amazon Assistant</a>
+                  <a href="">Help</a>
+               </div>
+               
+             </div>
+             <hr style="margin: 0; padding: 0;">
+               <div class="footer-pay">
+                     <img src="../images/logo.png" alt="">
+                     <button><i class="fas fa-globe"></i>English</button>
+                     <button><i class="fas fa-dollar-sign"></i>USD - U.S.Dollar</button>
+                     <button><img src="../images/logo2.png" alt="">United States</button>
+               </div>
+               <div class="footer-info2 d-flex">
+                  <div class="footer-info-text2" style="margin-left: 0rem;">
+                     <a href="">Amazon Music<br><h10>Stream millions of songs</h10></a><br><br>
+                     <a href="">Sell on Amazon<br><h10>Start a Selling Account</h10></a><br><br>
+                     <a href="">Book Depository<br><h10>Books With Free Delivery Worldwide</h10></a><br><br>
+                     <a href="">IMDb<br><h10>Movies, TV & Celebrities</h10></a><br><br>
+                     <a href="">Ring<br><h10>Smart Home Security Systerms</h10></a><br><br>
+                  </div>
+                  <div class="footer-info-text2" style="margin-left: 3rem;">
+                     <a href="">Amazon Advertising<br><h10>Find, attract, and enage customers</h10></a><br><br>
+                     <a href="">Amazon Business<br><h10>Everything for your Business</h10></a><br><br>
+                     <a href=""> Box Office Mojo<br><h10>Find movie Box Office Data</h10></a><br><br>
+                     <a href="">IMDbPro<br><h10>Get info Entertainment Professionals</h10></a><br><br>
+                     <a href="">erro WIFI<br><h10>Stream 4K Video in Every Room</h10></a><br><br>
+                  </div>
+                  <div class="footer-info-text2" style="margin-left: 3rem;">
+                     <a href="">Amazon Drive<br><h10>Cloud storage from Amazon</h10></a><br><br>
+                     <a href="">AmazonGolbale<br><h10>Ship Orders Internationally</h10></a><br><br>
+                     <a href="">ComiXology<br><h10>Thousands of Didital Comics</h10></a><br><br>
+                     <a href="">Kindle Direct Publishing<br><h10>Indie Diditals & Print Publishing Made Easy</h10></a><br><br>
+                     <a href="">Bling<br><h10>Smart Security for Every Home</h10></a><br><br>
+                  </div>
+                  <div class="footer-info-text2" style="margin-left: 3rem;">
+                     <a href="">6pm<br><h10>Stream millions of songs</h10></a><br><br>
+                     <a href="">Sell on Amazon<br><h10>Start a Selling Account</h10></a><br><br>
+                     <a href="">Book Depository<br><h10>Books With Free Delivery Worldwide</h10></a><br><br>
+                     <a href="">IMDb<br><h10>Movies, TV & Celebrities</h10></a><br><br>
+                     <a href="">Ring<br><h10>Smart Home Security Systerms</h10></a><br><br>
+                  </div>
+                  <div class="footer-info-text2" style="margin-left: 3rem;">
+                     <a href="">Amazon Music<br><h10>Stream millions of songs</h10></a><br><br>
+                     <a href="">Sell on Amazon<br><h10>Start a Selling Account</h10></a><br><br>
+                     <a href="">Book Depository<br><h10>Books With Free Delivery Worldwide</h10></a><br><br>
+                     <a href="">IMDb<br><h10>Movies, TV & Celebrities</h10></a><br><br>
+                     <a href="">Ring<br><h10>Smart Home Security Systerms</h10></a><br><br>
+                  </div>
+                  <div class="footer-info-text2" style="margin-left: 3rem;">
+                     <a href="">Amazon Musi<br>c<h10>Stream millions of songs</h10></a><br><br>
+                     <a href="">Sell on Amazon<br><h10>Start a Selling Account</h10></a><br><br>
+                     <a href="">Book Depository<br><h10>Books With Free Delivery Worldwide</h10></a><br><br>
+                     <a href="">IMDb<br><h10>Movies, TV & Celebrities</h10></a><br><br>
+                     <a href="">Ring<br><h10>Smart Home Security Systerms</h10></a><br><br>
+                  </div>
+                  <div class="footer-info-text2" style="margin-left: 3rem;">
+                     <a href="">Amazon Music<br><h10>Stream millions of songs</h10></a><br><br>
+                     <a href="">Sell on Amazon<br><h10>Start a Selling Account</h10></a><br><br>
+                     <a href="">Book Depository<br><h10>Books With Free Delivery Worldwide</h10></a><br><br>
+                     <a href="">IMDb<br><h10>Movies, TV & Celebrities</h10></a><br><br>
+                     <a href="">Ring<br><h10>Smart Home Security Systerms</h10></a><br><br>
+                  </div>
+               </div>
+               <div class="user-create">
+                  <a href="" style="margin-right: 5px;">Condition of Use</a>
+                  <a href=""  style="margin-right: 5px;">Privacy Notice</a>
+                  <a href=""  style="margin-right: 5px;">Interest-Based Ads</a>
+                  <h10>© 1996-2021, Amazon.com, Inc. or its affiliates</h10>
+               </div>
+            
+      </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+<script src="../js/all.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
